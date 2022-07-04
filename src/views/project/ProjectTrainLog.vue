69f247eee7acab34cbbe3e793e3dbf3fb1285672
lishengbo <1171734220@qq.com>
2022-07-04 15:57:41 +0800

    feat:日志实时滚动

--- src/views/project/ProjectTrainLog.vue
+++ src/views/project/ProjectTrainLog.vue
@@ -1,6 +1,14 @@
 <script setup>
-import { ref, reactive, computed, onUnmounted, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+import {
+  ref,
+  reactive,
+  computed,
+  onUnmounted,
+  onMounted,
+  watch,
+  nextTick,
+} from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 import { useFileData } from '@/stores';
 import { getTrainLog, autoEvaluate } from '@/api/api-project';
@@ -10,7 +18,17 @@ import IconStopped from '~icons/app/stopped';
 import IconRuning from '~icons/app/runing';
 import IconFailed from '~icons/app/failed';
 
+const showEvaBtn = ref(true);
+const isDisabled = ref(false);
+const showAnaButton = ref(false);
+const showGoButton = ref(false);
+const evaluateUrl = ref();
+
 const ruleRef = ref();
+
+const route = useRoute();
+const router = useRouter();
+
 const i18n = {
   title: '自动评估',
   desc: '训练日志可视化，请按顺序输入超参数范围，详细参考文档',
@@ -27,6 +45,7 @@ const query = reactive({
   momentum: '',
   batch_size: '',
 });
+
 const rules = reactive({
   learning_rate: [
     { required: true, message: '必填项', trigger: 'blur' },
@@ -54,7 +73,6 @@ const rules = reactive({
   ],
 });
 
-const route = useRoute();
 //训练日志js
 const form = reactive({
   name: '',
@@ -65,9 +83,6 @@ const form = reactive({
 const detailData = computed(() => {
   return useFileData().fileStoreData;
 });
-// const i18n = {
-//   recentDownload: '',
-// };
 
 // 训练日志详情数据
 const trainDetail = ref({});
@@ -82,11 +97,9 @@ function getTrainLogData() {
   };
   getTrainLog(trainLogParams).then((res) => {
     if (res.status === 200) {
-      console.log(res.data);
       form.desc = res.data.data.log.content;
       form.name = res.data.data.insance_name;
       trainDetail.value = res.data.data;
-      // console.log(trainDetail.value);
       if (trainDetail.value.metrics) {
         query.learning_rate = JSON.parse(
           trainDetail.value.metrics
@@ -100,7 +113,6 @@ function getTrainLogData() {
         isDisabled.value = true;
         showEvaBtn.value = false;
         timer = setInterval(() => {
-          // console.log(111);
           socket.send(
             JSON.stringify({
               pk: detailData.value.id,
@@ -110,7 +122,6 @@ function getTrainLogData() {
           );
         }, 1000);
         timer1 = setInterval(() => {
-          // console.log(222);
           socket.send(
             JSON.stringify({
               pk: detailData.value.id,
@@ -150,7 +161,6 @@ socket.onopen = function () {
 
 // 当websocket接收到服务端发来的消息时，自动会触发这个函数。
 socket.onmessage = function (event) {
-  // console.log(event.data);
   if (event.data.substring(0, 3) === 'log') {
     form.desc = event.data.substring(4);
   } else {
@@ -190,28 +200,11 @@ ws.onmessage = function (event) {
   evaluateUrl.value = JSON.parse(event.data).data.url;
 };
 
-// 跳到评估页面
-function goToPage() {
-  window.open( `${evaluateUrl.value}`)
-}
-
-const showEvaBtn = ref(true);
-const isDisabled = ref(false);
-const showAnaButton = ref(false);
-const showGoButton = ref(false);
-const evaluateUrl = ref();
 // 自动评估
 function saveSetting() {
-  // let params = {
-  //   learning_rate: [0.01],
-  //   momentum: [],
-  //   batch_size: [],
-  // };
   // ruleRef.value.validate((valid) => {
   // if (valid) {
-  // console.log(query);
   autoEvaluate(query, detailData.value.id, route.params.trainId).then((res) => {
-    // console.log(res);
     if (res.status === 200) {
       showEvaBtn.value = false;
       showAnaButton.value = true;
@@ -226,6 +219,25 @@ function saveSetting() {
   // });
 }
 
+// 跳到评估页面
+function goToPage() {
+  window.open(`${evaluateUrl.value}`);
+}
+
+// config.json详情
+function goJsonFile(file) {
+  router.push(
+    `/projects/${detailData.value.owner_name.name}/${detailData.value.name}/blob/${file}`
+  );
+}
+
+// 日志详情
+function goLogFile(){
+  router.push(
+    `/projects/${detailData.value.owner_name.name}/${detailData.value.name}/tree/train/log/`
+  );
+}
+
 onMounted(() => {
   window.addEventListener('beforeunload', () => reloadPage());
 });
@@ -235,6 +247,16 @@ onUnmounted(() => {
   clearInterval(timer);
   clearInterval(timer1);
 });
+
+watch(
+  () => form.desc,
+  () => {
+    const obj = document.querySelector('#txt');
+    nextTick(() => {
+      obj.scrollTop = obj.scrollHeight;
+    });
+  }
+);
 </script>
 <template>
   <div class="train-log">
@@ -249,7 +271,7 @@ onUnmounted(() => {
         </el-input>
       </div>
       <div class="train-log-desc">
-        <el-input v-model="form.desc" type="textarea" />
+        <el-input id="txt" v-model="form.desc" type="textarea" />
       </div>
     </div>
     <div class="train-log-detail">
@@ -299,14 +321,17 @@ onUnmounted(() => {
           </li>
           <li class="info-list">
             <div class="info-list-title">输入参数文件</div>
-            <div class="info-list-detail document">
+            <div
+              @click="goJsonFile(trainDetail.config_path)"
+              class="info-list-detail document"
+            >
               {{ trainDetail.config_path }}
             </div>
           </li>
           <li class="info-list">
             <div class="info-list-title">日志文件</div>
             &nbsp;
-            <div class="info-list-detail document">
+            <div @click="goLogFile" class="info-list-detail document">
               {{ trainDetail.log_file }}
             </div>
           </li>
@@ -409,6 +434,25 @@ onUnmounted(() => {
           height: 100%;
         }
       }
+      :deep #txt {
+        &::-webkit-scrollbar {
+          width: 6px;
+          height: 6px;
+        }
+
+        &::-webkit-scrollbar-thumb {
+          border-radius: 3px;
+          background-color: #d8d8d8;
+          background-clip: content-box;
+        }
+
+        &::-webkit-scrollbar-track {
+          border-radius: 3px;
+          box-shadow: inset 0 0 2px rgba($color: #000000, $alpha: 0.2);
+          background: #ffffff;
+          // background:transparent;
+        }
+      }
     }
   }
   &-detail {
@@ -480,6 +524,7 @@ onUnmounted(() => {
           overflow: hidden;
           text-overflow: ellipsis;
           white-space: nowrap;
+          cursor: pointer;
         }
       }
       // .info-btn {

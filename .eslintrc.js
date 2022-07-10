831167c4b84ea8e0d25a8c95fdd7c7f71e3b92a9
Bang <15622356989@163.com>
2022-07-10 18:31:07 +0800

    fix: 强制检测代码规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-var': 'error',
+    eqeqeq: 'error',
     'no-unused-vars': 'error',
   },
 };

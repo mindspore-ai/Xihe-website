8556255efcd9bc4442d0ada8cf3ec1cb636cf3d4
haml-707 <845831435@qq.com>
2022-09-06 09:43:44 +0800

    feat:gitlab 相关api 接口

--- src/api/api-gitlab.js
+++ src/api/api-gitlab.js
@@ -18,8 +18,58 @@ function getHeaderConfig() {
 }
 
 export function getGitlabToken() {
-  const url = `/server/user/gzbang/gitlab`;
+  const url = `/server/888/gitlab`;
   return request.get(url, getHeaderConfig()).then((res) => {
+    return res.data;
+  });
+}
+
+export function creatModelRepo() {
+  const url = `/server/model`;
+  const params = {
+    desc: '描述',
+    name: 'firstModel',
+    owner: '888',
+    protocol: 'mit',
+    repo_type: 'public',
+  };
+  return request.post(url, params, getHeaderConfig()).then((res) => {
+    return res.data;
+  });
+}
+
+export function getModelById() {
+  const url = '/server/model/888/6315f1007187a3b38b417638';
+  return request.get(url, getHeaderConfig()).then((res) => {
+    return res.data;
+  });
+}
+
+export function uploadFileGitlab() {
+  const url = `/repo/projects/2/repository/files/${encodeURIComponent(
+    'app%2Fproject%2Erb2'
+  )}`;
+  const headers = {
+    'PRIVATE-TOKEN': 'hGq8ze9XF6VDsis2t4SY',
+  };
+  const data = {
+    branch: 'main',
+    author_email: '845831435@qq.com',
+    author_name: '888',
+    content: '第一文件',
+    commit_message: 'commit_message',
+  };
+  return request.post(url, data, { headers }).then((res) => {
+    return res.data;
+  });
+}
+
+export function getGitlabTree() {
+  const url = '/repo/projects/2/repository/tree';
+  const headers = {
+    'PRIVATE-TOKEN': 'hGq8ze9XF6VDsis2t4SY',
+  };
+  return request.get(url, { headers }).then((res) => {
     return res.data;
   });
 }

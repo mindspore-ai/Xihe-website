6443d44f2771b56d18f08183ee33a271961809d6
yyj <1581991552@qq.com>
2022-12-16 17:05:02 +0800

    perf:悟空封面

--- src/views/modelzoo/wukong/WukongAlbum.vue
+++ src/views/modelzoo/wukong/WukongAlbum.vue
@@ -98,129 +98,6 @@ const imgs = reactive([
 </template>
 
 <style lang="scss" scoped>
-// TODO:
-:deep(.el-dialog) {
-  // background: rgba(0, 0, 0, 0.85);
-  // padding-left: 6%;
-  // padding-right: 6%;
-  .el-dialog__header {
-    height: 80px;
-    // width: 100%;
-    position: fixed;
-    z-index: 10;
-    background: #000000;
-    margin-right: 0px;
-    padding-bottom: 18px;
-    span {
-      height: 24px;
-      line-height: 24px;
-      font-size: 24px;
-      color: #ffffff;
-      // position: fixed;
-    }
-  }
-  .el-dialog__body {
-    padding-top: 40px;
-    // padding-left: 6%;
-    // padding-right: 6%;
-    // background: rgba(0, 0, 0, 0.85);
-    .picture-album {
-      .album-list {
-        .album-item1 {
-          .img-box {
-            &:first-child {
-              grid-area: 1 / 1 / 3 / 3;
-            }
-          }
-        }
-        .album-item2 {
-          .img-box {
-            &:nth-child(2) {
-              grid-area: 1 / 2 / 3 / 4;
-            }
-          }
-        }
-        .album-item3 {
-          .img-box {
-            &:nth-child(3) {
-              grid-area: 1 / 3 / 3 / 5;
-            }
-          }
-        }
-        .album-item4 {
-          .img-box {
-            &:nth-child(4) {
-              grid-area: 1 / 4 / 3 / 6;
-            }
-          }
-        }
-        .album-item1,
-        .album-item2,
-        .album-item3,
-        .album-item4 {
-          width: 100%;
-          display: grid;
-          grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-          // grid-template-columns: repeat(4, 100px);
-          column-gap: 24px;
-          row-gap: 24px;
-          margin-bottom: 24px;
-          .img-box {
-            background-color: #bfa;
-            position: relative;
-            cursor: pointer;
-            &:hover {
-              .imgs-info {
-                display: block;
-              }
-            }
-            // &:first-child {
-            //   grid-area: 1 / 1 / 3 / 3;
-            // }
-            img {
-              width: 100%;
-              height: 100%;
-            }
-            .imgs-info {
-              display: none;
-              width: 100%;
-              font-size: 18px;
-              color: #ffffff;
-              position: absolute;
-              bottom: 24px;
-              left: 0;
-              padding: 0 24px;
-              .style {
-                margin-bottom: 10px;
-                white-space: nowrap;
-                overflow: hidden;
-                text-overflow: ellipsis;
-              }
-              .source {
-                white-space: nowrap;
-                overflow: hidden;
-                text-overflow: ellipsis;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  .el-dialog__headerbtn {
-    position: fixed;
-    right: 16px;
-    .el-dialog__close {
-      width: 48px;
-      height: 48px;
-      color: #fff;
-      svg {
-        font-size: 24px;
-      }
-    }
-  }
-}
-
 .picture-album {
   .album-list {
     .album-item1 {
@@ -263,7 +140,7 @@ const imgs = reactive([
       row-gap: 24px;
       margin-bottom: 24px;
       .img-box {
-        background-color: #bfa;
+        // background-color: #bfa;
         position: relative;
         cursor: pointer;
         &:hover {

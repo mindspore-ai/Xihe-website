996161131e100e957f3194c94615f0379c0b6282
lishengbo <1171734220@qq.com>
2022-12-19 16:35:53 +0800

    feat: trainlog输出下载报错处理

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-import cesium from 'vite-plugin-cesium';
+// import cesium from 'vite-plugin-cesium';
 
 import Icons from 'unplugin-icons/vite';
 import { FileSystemIconLoader } from 'unplugin-icons/loaders';
@@ -27,7 +27,7 @@ export default defineConfig({
     },
   },
   plugins: [
-    cesium(),
+    // cesium(),
     vue(),
     AutoImport({
       resolvers: [ElementPlusResolver()],

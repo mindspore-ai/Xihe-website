0aae072da40d9c12c23b298e6252485d3f72ec7c
@popular-male-star <3197561863@qq.com>
2023-02-10 11:20:58 +0800

    feat：比赛介绍支持latex公式

--- src/views/competition/CompetitionAgreement.vue
+++ src/views/competition/CompetitionAgreement.vue
@@ -1,6 +1,24 @@
 <script setup>
 import agreement from '@/assets/statement/competition_agreement.md?raw';
 import MdStatement from '@/components/MdStatement.vue';
+
+import { onUpdated } from 'vue';
+
+const TypeSet = async function (elements) {
+  if (!window.MathJax) {
+    return;
+  }
+  window.MathJax.startup.promise = window.MathJax.startup.promise
+    .then(() => {
+      return window.MathJax.typesetPromise(elements);
+    })
+    .catch((err) => console.log('Typeset failed: ' + err.message));
+  return window.MathJax.startup.promise;
+};
+onUpdated(() => {
+  const a = document.querySelectorAll('p');
+  TypeSet(a);
+});
 </script>
 <template>
   <div class="agreement">

145023844621240230ad4cc71121ade1235684c0
yyj <1581991552@qq.com>
2023-03-24 11:26:26 +0800

    feat:课程证书

--- src/views/course/CourseCertificate.vue
+++ src/views/course/CourseCertificate.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, onMounted, nextTick } from 'vue';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import html2canvas from 'html2canvas';
 
 import emptyImg from '@/assets/imgs/model-empty.png';
@@ -7,64 +8,81 @@ import emptyImg from '@/assets/imgs/model-empty.png';
 import { getCertificate } from '@/api/api-course';
 import { useCourseData } from '@/stores';
 
-const certificate = ref(null);
+const router = useRouter();
+
+const certificateRef = ref(null);
 const certificateData = ref({});
 const courseInfo = useCourseData().courseData;
-getCertificate(courseInfo.id).then((res) => {
-  certificateData.value = res.data;
-  console.log('certificateData.value: ', certificateData.value);
-});
+const show = ref(false);
 
-// const certificateUrl = ref('');
-// 生成证书
-function generateCertificate() {
-  // const certificateDiv = document.querySelector('#certificate');
-  const certificateDiv = certificate.value;
-  nextTick(() => {
-    html2canvas(certificateDiv).then((canvas) => {
-      const dataUrl = canvas.toDataURL('image/png');
-      // certificateUrl.value = dataUrl;
-      // const img = new Image();
-      // img.src = dataUrl;
-      // document.body.appendChild(img);
-      // const link = document.createElement('a');
-      // link.download = 'certificate.png';
-      // link.href = dataUrl;
-      // link.click();
-      // link.remove();
+function gainCertificate() {
+  if (courseInfo.is_apply) {
+    getCertificate(courseInfo.id).then((res) => {
+      certificateData.value = res.data;
+      show.value = true;
     });
+  } else {
+    router.push(`/course/${courseInfo.id}`);
+  }
+}
+gainCertificate();
+const loadDone = ref(false);
+const certRef = ref(null);
+// const certNewvalue = ref('');
+// 生成证书
+const certificateUrl = ref('');
+function generateCertificate() {
+  const certificateDiv = certificateRef.value;
+  html2canvas(certificateDiv).then((canvas) => {
+    const dataUrl = canvas.toDataURL('image/png');
+    certificateUrl.value = dataUrl;
+    // console.log('dataUrl: ', dataUrl);
+    certificateRef.value.style.display = 'none';
   });
 }
-onMounted(() => {
+function loadImg() {
+  loadDone.value = true;
   generateCertificate();
-});
+}
 </script>
 <template>
-  <div class="course-certificate">
-    <div
-      v-if="!certificateData.is_pass"
-      ref="certificate"
-      class="certificate-img"
-    >
-      <p>123{{ certificateData.owner }}</p>
-      <img :src="certificateData.cert" alt="" />
-      <!-- <img src="@/assets/imgs/logo.png" alt="" /> -->
-    </div>
-    <div v-else class="empty">
+  <div v-if="show" class="course-certificate">
+    <img
+      v-if="certificateData.is_pass && certificateUrl"
+      :src="certificateUrl"
+      alt=""
+      class="certificate-loadImg"
+    />
+    <div v-if="!certificateData.is_pass" class="empty">
       <img :src="emptyImg" alt="" />
       <p>暂未获得节课证书，继续努力吧～</p>
     </div>
-    <!-- <div v-if="certificateUrl">
-      <img :src="certificateUrl" alt="" />
-    </div> -->
+    <div
+      v-if="certificateData.is_pass"
+      ref="certificateRef"
+      class="certificate-img"
+    >
+      <p>123{{ certificateData.owner }}</p>
+      <img
+        ref="certRef"
+        src="@/assets/imgs/course/cert.png"
+        alt=""
+        @load="loadImg"
+      />
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .course-certificate {
   overflow: hidden;
+  .certificate-loadImg {
+    width: 100%;
+  }
   .certificate-img {
     position: relative;
+    // display: none;
+    // opacity: 0;
     img {
       width: 100%;
       // height: 640px;
---
+++ src/views/course/CourseTaskDetail.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { ref, computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { ArrowRight } from '@element-plus/icons-vue';
+
+import { getTaskDetail } from '@/api/api-course';
+
+const route = useRoute();
+console.log('route: ', route.params);
+// console.log('route: ', route.params.chapterId);
+const router = useRouter();
+function getTask() {
+  getTaskDetail(route.params.courseId, route.params.taskId).then((res) => {
+    console.log('res: ', res);
+  });
+}
+// getTask();
+</script>
+<template>
+  <div class="task-detail">
+    <div class="task-detail-wrap">
+      <div class="task-bread">
+        <el-breadcrumb :separator-icon="ArrowRight">
+          <el-breadcrumb-item :to="{ path: '/course' }">
+            课程
+          </el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: `/` }">
+            AI大模型公开课
+          </el-breadcrumb-item>
+          <el-breadcrumb-item> chapterData.name </el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <div class="task-content">
+        <div class="task-name">dfs</div>
+        <div class="task-introduce">dfds</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.task-detail {
+  max-width: 100%;
+  min-height: calc(100vh - 200px);
+  margin-top: 80px;
+  background-color: #f5f6f8;
+  .task-detail-wrap {
+    max-width: 1440px;
+    height: 100%;
+    padding: 40px 0px 64px;
+    margin: 0 auto;
+    .task-bread {
+      .el-breadcrumb {
+        height: 21px;
+        line-height: 21px;
+        font-size: 12px;
+        .el-breadcrumb__item {
+          :deep(.el-breadcrumb__inner.is-link) {
+            color: #555;
+            font-weight: 400;
+            &:hover {
+              color: #0d8dff;
+            }
+          }
+          :deep(.el-breadcrumb__separator.el-icon) {
+            color: #555;
+          }
+        }
+        :deep(.el-breadcrumb__item:last-child .el-breadcrumb__inner) {
+          color: #000;
+        }
+      }
+    }
+    .task-content {
+      .task-name {
+        line-height: 48px;
+        font-size: 36px;
+        color: #000000;
+        margin: 40px 0;
+      }
+      .task-introduce {
+        height: 865px;
+        padding: 40px;
+        background: #ffffff;
+        box-shadow: 0px 1px 5px 0px rgba(45, 47, 51, 0.1);
+      }
+    }
+  }
+}
+</style>

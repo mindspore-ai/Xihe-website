ba419f7e7009a063cc4643575155cb43e4467b76
@popular-male-star <3197561863@qq.com>
2023-03-27 21:29:00 +0800

    fix：首页

--- src/views/project/TheProject.vue
+++ src/views/project/TheProject.vue
@@ -32,7 +32,7 @@ const router = useRouter();
 
 let i18n = {
   head: {
-    title: '项目',
+    title: 'AI实验室',
     introduce:
       '覆盖多领域任务，体验全流程开发，支持用户在线训练和推理可视化，可创建自己的项目空间，详情请点击',
     reference: '参考文档',

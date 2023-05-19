35fd7c1653ecc5c661e39fce33426653ef4b15ce
yyj <1581991552@qq.com>
2023-05-19 15:38:47 +0800

    feat:训练新视觉

--- src/views/project/ProjectTrain.vue
+++ src/views/project/ProjectTrain.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch, computed, onUpdated,onUnmounted  } from 'vue';
+import { ref, watch, computed, onUpdated, onUnmounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 import { handleMarkdown } from '@/shared/markdown';
@@ -79,8 +79,8 @@ const userInfo = useUserInfoStore();
 const detailData = computed(() => {
   return useFileData().fileStoreData;
 });
-const isShow = ref(false);
-const isShow1 = ref(false);
+const isShowDatasetDlg = ref(false);
+const isShowModelDlg = ref(false);
 const addSearch = ref('');
 const showTip = ref(false);
 
@@ -161,7 +161,7 @@ function goSelectFile() {
 
 // 点击新增数据集
 function addRelateClick() {
-  isShow.value = true;
+  isShowDatasetDlg.value = true;
 }
 const emit = defineEmits(['on-click']);
 
@@ -216,12 +216,12 @@ function confirmAdd() {
             message: '添加成功',
           });
           emit('on-click');
-          isShow.value = false;
+          isShowDatasetDlg.value = false;
           addSearch.value = '';
         }
       })
       .catch(() => {
-        isShow.value = false;
+        isShowDatasetDlg.value = false;
         addSearch.value = '';
         ElMessage({
           type: 'error',
@@ -273,11 +273,11 @@ function confirmClick() {
           message: '添加成功',
         });
         emit('on-click');
-        isShow1.value = false;
+        isShowModelDlg.value = false;
         addSearch.value = '';
       })
       .catch(() => {
-        isShow1.value = false;
+        isShowModelDlg.value = false;
         addSearch.value = '';
         ElMessage({
           type: 'error',
@@ -317,7 +317,7 @@ function deleteClick(item) {
 }
 
 function addModeClick() {
-  isShow1.value = true;
+  isShowModelDlg.value = true;
 }
 // 获取README文件
 function getReadMeFile() {
@@ -517,7 +517,7 @@ watch(
 
     <!-- 添加数据集弹窗 -->
     <el-dialog
-      v-model="isShow"
+      v-model="isShowDatasetDlg"
       width="640px"
       :show-close="false"
       center
@@ -539,16 +539,22 @@ watch(
       </el-form>
       <template #footer>
         <span class="dialog-footer">
-          <o-button style="margin-right: 16px" @click="isShow = false"
-            >取消</o-button
+          <o-button
+            size="small"
+            style="margin-right: 16px"
+            @click="isShowDatasetDlg = false"
           >
-          <o-button type="primary" @click="confirmAdd">确定</o-button>
+            取消
+          </o-button>
+          <o-button size="small" type="primary" @click="confirmAdd">
+            确定
+          </o-button>
         </span>
       </template>
     </el-dialog>
     <!-- 添加模型弹窗 -->
     <el-dialog
-      v-model="isShow1"
+      v-model="isShowModelDlg"
       width="640px"
       :show-close="false"
       center
@@ -570,10 +576,15 @@ watch(
       </el-form>
       <template #footer>
         <span class="dialog-footer">
-          <o-button style="margin-right: 16px" @click="isShow1 = false"
+          <o-button
+            size="small"
+            style="margin-right: 16px"
+            @click="isShowModelDlg = false"
             >取消</o-button
           >
-          <o-button type="primary" @click="confirmClick">确定</o-button>
+          <o-button size="small" type="primary" @click="confirmClick"
+            >确定</o-button
+          >
         </span>
       </template>
     </el-dialog>
@@ -704,11 +715,13 @@ watch(
         font-size: 14px;
         color: #555555;
         line-height: 24px;
+        font-weight: normal;
       }
       .download-count {
         font-size: 18px;
         color: #000000;
         line-height: 24px;
+        font-weight: normal;
       }
     }
     .card-top {
@@ -744,42 +757,6 @@ watch(
         grid-template-columns: repeat(1, minmax(200px, 1fr));
         column-gap: 24px;
         row-gap: 24px;
-        .dataset-item {
-          width: 100%;
-          padding: 24px;
-          background-color: #fff;
-          box-shadow: 0px 1px 5px 0px rgba(45, 47, 51, 0.1);
-          position: relative;
-          &:hover {
-            box-shadow: 0px 6px 18px 0px rgba(13, 141, 255, 0.14);
-            .remove-item {
-              display: block;
-            }
-          }
-          .remove-item {
-            position: absolute;
-            right: 8px;
-            top: 8px;
-            display: none;
-          }
-          .dataset-top {
-            margin-bottom: 14px;
-          }
-          .dataset-bottom {
-            display: flex;
-            justify-content: start;
-            align-items: center;
-            & > div {
-              display: flex;
-              align-items: center;
-              margin-right: 24px;
-              span {
-                font-size: 20px;
-                padding-right: 4px;
-              }
-            }
-          }
-        }
       }
     }
     .add-title {
@@ -791,6 +768,7 @@ watch(
       margin: 48px 0 24px;
       font-size: 18px;
       line-height: 24px;
+      font-weight: normal;
     }
     .add {
       margin: 48px 0 24px;

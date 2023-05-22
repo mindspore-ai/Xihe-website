d0b10ddd391ac4969489270c7066a5d91b897304
@popular-male-star <3197561863@qq.com>
2023-05-22 15:03:20 +0800

    fix：绑定邮箱弹窗

--- src/shared/axios/index.js
+++ src/shared/axios/index.js
@@ -3,7 +3,7 @@ import handleResponse from './handleResponse';
 import handleError from './handleError';
 import setConfig from './setConfig';
 
-import { useLoadingState } from '@/stores/index';
+import { useLoadingState, useDialogState } from '@/stores/index';
 
 import { ElLoading } from 'element-plus';
 import { goAuthorize, saveUserAuth } from '@/shared/login';
@@ -92,6 +92,10 @@ const responseInterceptorId = request.interceptors.response.use(
       if (err.status === 401) {
         saveUserAuth();
         goAuthorize();
+      }
+      if (err.response.data.code === 'user_no_email') {
+        useDialogState().dialogState = true;
+        config.$doException = true;
       }
     } else {
       // 没有response(没有状态码)的情况

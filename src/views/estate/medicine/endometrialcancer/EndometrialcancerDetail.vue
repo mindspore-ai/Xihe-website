07de7376dc721f22e5fae4787f39f9dd43af8b56
yyj <1581991552@qq.com>
2023-06-13 20:37:48 +0800

    feat:产业移动端

--- src/views/estate/medicine/endometrialcancer/EndometrialcancerDetail.vue
+++ src/views/estate/medicine/endometrialcancer/EndometrialcancerDetail.vue
@@ -1,36 +1,18 @@
 <script setup>
 import { ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import endometrialcancerCover from '@/assets/imgs/estate/medicine/cancer/cancer-cover.png';
 
 import IconArrowRight from '~icons/app/arrow-right.svg';
+import IconLeft from '~icons/app/left.svg';
+
 import { ArrowRight } from '@element-plus/icons-vue';
+import useWindowResize from '@/shared/hooks/useWindowResize.js';
 
 const route = useRoute();
-
+const router = useRouter();
 const activeNavItem = ref('');
-
-// TODO:
-/* const navItems = reactive([
-  {
-    id: 'projectExplain',
-    label: '项目说明',
-    href: 'project-explain',
-    isIndividual: true,
-  },
-  {
-    id: 'dataPrepare',
-    label: '数据准备',
-    href: 'data-prepare',
-    isIndividual: true,
-  },
-  {
-    id: 'modelTrain',
-    label: '模型训练',
-    href: 'model-train',
-    isIndividual: false,
-  },
-]); */
+const screenWidth = useWindowResize();
 
 watch(
   () => {
@@ -50,10 +32,17 @@ watch(
 /* function handleNavClick(item) {
   router.push(`/estate/electric/case-1/${item.href}`);
 } */
+function goBack() {
+  router.push('/medicine');
+}
 </script>
 
 <template>
   <div class="medicine-detail">
+    <div v-if="screenWidth < 820" class="navigation">
+      <o-icon class="turn" @click="goBack"><icon-left></icon-left></o-icon>
+      <span class="title">子宫内膜癌筛查联合方案</span>
+    </div>
     <div class="medicine-wrap">
       <div class="bread-wrap">
         <el-breadcrumb :separator-icon="ArrowRight">
@@ -79,7 +68,13 @@ watch(
               </div>
             </div>
             <div class="banner-btn">
-              <OButton disabled type="primary" animation class="home-btn">
+              <OButton
+                :size="screenWidth < 820 ? 'mini' : 'small'"
+                disabled
+                type="primary"
+                animation
+                class="home-btn"
+              >
                 运行模型
                 <template #suffix>
                   <OIcon><IconArrowRight /></OIcon>
@@ -89,14 +84,6 @@ watch(
           </div>
         </div>
         <div class="medicine-content-desc">
-          <!-- TODO: -->
-          <!-- <div class="industry-tab">
-            <o-nav
-              :nav-items="navItems"
-              :active-item="activeNavItem"
-              @nav-click="handleNavClick"
-            ></o-nav>
-          </div> -->
           <div class="industry-info">
             <router-view />
           </div>
@@ -111,40 +98,41 @@ watch(
   background-color: #f5f6f8;
   padding-top: 80px;
   min-height: calc(100vh - 200px);
-
+  @media screen and (max-width: 820px) {
+    padding: 48px 0px 40px;
+  }
+  .navigation {
+    font-size: 16px;
+    color: #000;
+    line-height: 48px;
+    padding: 0 16px;
+    margin-bottom: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff;
+    box-shadow: 0 3px 8px rgba(0, 0, 0, 0.0509803922);
+    position: relative;
+    .o-icon {
+      position: absolute;
+      left: 16px;
+    }
+  }
   .medicine-wrap {
     padding: 0px 16px 64px;
     margin: 0 auto;
     max-width: 1448px;
     overflow: hidden;
-    .bread-wrap {
-      // height: 94px;
-      // padding-top: 40px;
-      // padding-bottom: 40px;
-      padding: 40px 0;
-      // background-color: #f5f6f8;
-      .el-breadcrumb {
-        font-size: 12px;
-        line-height: 18px;
-        .el-breadcrumb__item {
-          :deep(.el-breadcrumb__inner.is-link) {
-            color: #555;
-            font-weight: 400;
-            &:hover {
-              color: #0d8dff;
-            }
-          }
-          :deep(.el-breadcrumb__separator.el-icon) {
-            color: #555;
-          }
+    @media screen and (max-width: 820px) {
+      padding-bottom: 0px;
+      .bread-wrap {
+        @media screen and (max-width: 820px) {
+          display: none;
         }
-        :deep(.el-breadcrumb__item:last-child .el-breadcrumb__inner) {
-          color: #000;
-        }
-        // :deep(.el-breadcrumb__item:nth-child(2) .el-breadcrumb__inner) {
-        //   cursor: pointer;
-        // }
       }
+    }
+    .bread-wrap {
+      padding: 40px 0;
     }
     .medicine-content {
       .medicine-content-banner {
@@ -153,9 +141,16 @@ watch(
         display: flex;
         border-radius: 16px;
         margin-bottom: 37px;
+        @media screen and (max-width: 820px) {
+          padding: 16px;
+          margin-bottom: 16px;
+        }
         .banner-left {
           width: 416px;
           margin-right: 40px;
+          @media screen and (max-width: 820px) {
+            display: none;
+          }
           img {
             width: 100%;
             height: 100%;
@@ -166,20 +161,34 @@ watch(
           display: flex;
           flex-direction: column;
           justify-content: space-between;
+          @media screen and (max-width: 820px) {
+            width: 100%;
+            display: flex;
+          }
           .banner-content {
             .banner-title {
               line-height: 48px;
               font-size: 36px;
               color: #000000;
               margin-bottom: 16px;
+              @media screen and (max-width: 820px) {
+                font-size: 16px;
+                line-height: 24px;
+                margin-bottom: 8px;
+              }
             }
             .banner-desc {
               font-size: 14px;
               color: #555555;
               line-height: 22px;
+              @media screen and (max-width: 820px) {
+                font-size: 12px;
+                line-height: 18px;
+              }
             }
           }
           .banner-btn {
+            margin-top: 16px;
             .o-button {
               border-radius: 24px;
             }
@@ -198,7 +207,6 @@ watch(
           height: 48px;
           box-shadow: 0px 1px 5px 0px rgb(45 47 51 / 10%);
           border-radius: 16px 16px 0 0;
-          // background-color: #fbfbfb;
           :deep(.o-nav) {
             width: 100%;
             display: flex;
@@ -211,8 +219,10 @@ watch(
           }
         }
         .industry-info {
-          // background-color: #f5f6f8;
           padding: 40px 80px;
+          @media screen and (max-width: 820px) {
+            padding: 16px;
+          }
         }
       }
     }

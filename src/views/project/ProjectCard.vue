06f54b22431f83dd4f31755c4a3e2afc81d1216f
lishengbo <1171734220@qq.com>
2023-07-14 17:24:12 +0800

    feat: 去除注释和登录失败报错

--- src/views/project/ProjectCard.vue
+++ src/views/project/ProjectCard.vue
@@ -288,12 +288,7 @@ watch(
     immediate: true,
   }
 );
-// watch(
-//   () => route.path,
-//   () => {
-//     location.reload();
-//   }
-// );
+
 function goEditor() {
   pushParams.contents = ['README.md'];
   router.push({
